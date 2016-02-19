71cb58d0124fce1ec5ced1a9d02772b35eb78f0e
archer <1246506786@qq.com>
2016-02-19 15:55:36 +0800

    QueryBookInfo tool ok

--- tool/QueryBookInfo.py
+++ tool/QueryBookInfo.py
@@ -8,7 +8,18 @@
 # Usage: ./QueryBookInfo.py isbn
 # Produced By BR
 import sys
+from pymongo import MongoClient
 
-if len(sys) != 2:
+if len(sys.argv) != 2:
     print "Usage: ./QueryBookInfo.py isbn"
     sys.exit(1)
+
+isbn = sys.argv[1]
+
+client = MongoClient('mongodb://linyy:rioreader@192.168.200.20/bookshelf')
+db = client['bookshelf']
+collection = db['bookful']
+
+record = collection.find_one({'isbn13': isbn})
+print record
+client.close()
