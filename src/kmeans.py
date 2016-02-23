eb698369b9d1d313e3d9e7ac64d7e29a4ef7bc63
archer <1246506786@qq.com>
2016-02-23 14:07:15 +0800

    can use kmeans now, but dont know the results mean

--- src/kmeans.py
+++ src/kmeans.py
@@ -10,15 +10,22 @@
 import sys
 from sklearn.cluster import KMeans
 
+from Utility import loadMatrixFromFile
+
 if len(sys.argv) != 2:
     print 'Usage: ./kmeans.py inputfile'
     sys.exit(1)
 
 inputfile = sys.argv[1]
 
+mat = loadMatrixFromFile(inputfile)
+nmat = []
+for row in mat:
+    nmat.append(row[1:])
+
 num_clusters = 120  # douban's book have 120 categories
 
-# km = KMeans(n_clusters=num_clusters)
-# km.fit(tfidf_matrix)
-# clusters = km.labels_.tolist()
-# print clusters
+km = KMeans(n_clusters=num_clusters)
+km.fit(nmat)
+clusters = km.labels_.tolist()
+print clusters
